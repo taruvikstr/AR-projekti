aeebaf57d559779c0130fe5f88bddc334e069129
Taru Vikström <taru.vikstrom@gmail.com>
2023-03-15 17:09:47 +0200

    dragoon

--- AR_projekti/Assets/DragonController.cs
+++ AR_projekti/Assets/DragonController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class DragonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private FixedJoystick fixedJoystick;
+    private Rigidbody rigidBody;
+
+    private void OnEnable()
     {
-        
+        fixedJoystick = FindObjectOfType<FixedJoystick>();
+        rigidBody = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float xVal = fixedJoystick.Horizontal;
+        float yVal = fixedJoystick.Vertical;
+
+        Vector3 movement = new Vector3(xVal, 0, yVal);
+        rigidBody.velocity = movement * speed;
+
+        if(xVal != 0 && yVal != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
     }
 }
